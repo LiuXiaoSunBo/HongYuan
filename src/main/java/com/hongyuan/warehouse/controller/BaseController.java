0712d20fc11432d772bdb6ac2bf6c0eb5e6ad2fb
ObjectTest <1152650793@qq.com>
2019-10-12 09:21:48 +0800

    fix url & fix encoding

--- src/main/java/com/hongyuan/warehouse/controller/BaseController.java
+++ src/main/java/com/hongyuan/warehouse/controller/BaseController.java
@@ -4,6 +4,7 @@ import com.hongyuan.warehouse.pojo.wechat.WechatTalkMessage;
 import com.hongyuan.warehouse.services.WechatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,7 +19,10 @@ public class BaseController {
     @Autowired
     WechatService wechatService;
     @RequestMapping("/")
-    public String test(HttpServletRequest request) throws IOException, URISyntaxException {
+    public String test(HttpServletRequest request, @RequestParam(value="id",required = false,defaultValue = "") String echostr) throws IOException, URISyntaxException {
+        if (echostr!=""&&echostr!=null){
+            return echostr;
+        }
         return wechatService.event(request);
     }
 }
